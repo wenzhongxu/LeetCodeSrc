df1d8b2f4ed70f6d12e199435ad26ca0b8680e27
wenzhongxu <xuwenzhong1994@163.com>
2019-05-26 21:16:53 +0800

    project add

---
+++ LeetCodeSrc/AlgorithmOther/SearchAlgorithm.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmOther
+{
+    /// <summary>
+    /// 一些日常学习中碰到的查找算法
+    /// </summary>
+    public class SearchAlgorithm
+    {
+        /// <summary>
+        /// 二分查找算法
+        /// </summary>
+        /// <param name="arr">原数组</param>
+        /// <param name="key">查找目标值</param>
+        /// <returns>目标查找值所在位置下标，找不到则返回-1</returns>
+        public int BinarySearch(int[] arr, int key)
+        {
+            int low = 0; //定义最低下标为记录首位
+            int high = arr.Length; // 定义最高下标为记录末位
+
+            while (low <= high)
+            {
+                int mid = (low + high) / 2; // 折半
+                if (key < arr[mid])
+                {
+                    high = mid - 1;
+                }
+                else if (key > arr[mid])
+                {
+                    low = mid + 1;
+                }
+                else
+                {
+                    return mid;
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ LeetCodeSrc/AlgorithmOther/SortAlgorithm.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmOther
+{
+    /// <summary>
+    /// 一些日常学习中碰到的排序算法
+    /// </summary>
+    public class SortAlgorithm
+    {
+        /// <summary>
+        /// 快排
+        /// 核心思想：两端向中间比较，并交换顺序
+        /// 不稳定，O(nlog2n)
+        /// </summary>
+        /// <param name="arr">待排序数组</param>
+        /// <param name="left">左边界</param>
+        /// <param name="right">右边界</param>
+        public void QuickSort(int[] arr, int left, int right)
+        {
+            if (left < right)
+            {
+                int i = left;
+                int j = right;
+                int x = arr[i];
+
+                while (i < j)
+                {
+                    while (i < j && arr[j] > x)
+                    {
+                        j--; // 从右向左找第一个小于x的数
+                    }
+                    if (i < j)
+                    {
+                        arr[i++] = arr[j];
+                    }
+                    while (i < j && arr[i] < x)
+                    {
+                        i++; // 从左向右找第一个大于x的数
+                    }
+                    if (i < j)
+                    {
+                        arr[j--] = arr[i];
+                    }
+                }
+
+                arr[i] = x;
+                QuickSort(arr, left, i - 1); // 递归调用
+                QuickSort(arr, i + 1, right); // 递归调用
+            }
+        }
+    }
+}

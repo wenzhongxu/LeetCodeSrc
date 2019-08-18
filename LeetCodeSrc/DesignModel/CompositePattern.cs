db578ade7464d7ed8acccbb69a5a830c4185df89
wenzhongxu <xuwenzhong1994@163.com>
2019-08-18 16:15:59 +0800

    DecoratorPattern and CompositePattern add

---
+++ LeetCodeSrc/DesignModel/CompositePattern.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignModel
+{
+    /*
+     * 优点
+     *  组合模式使得客户端代码可以一致地处理对象和对象容器，无需关心处理的单个对象，还是组合的对象容器。
+     *  将“客户代码与复杂的对象容器结构”解耦。
+     *  可以更容易地往组合对象中加入新的构件。
+     * 缺点：使得设计更加复杂。客户端需要花更多时间理清类之间的层次关系。
+     */
+
+
+    /// <summary>
+    /// 图形抽象类
+    /// </summary>
+    public abstract class Graphics
+    {
+        public string Name { get; set; }
+
+        public Graphics(string name)
+        {
+            this.Name = name;
+        }
+
+        public abstract void Draw();
+    }
+
+    /// <summary>
+    /// 简单图形类——线
+    /// </summary>
+    public class Line : Graphics
+    {
+        public Line(string name) : base(name)
+        {
+
+        }
+
+        // 重写父类抽象方法
+        public override void Draw()
+        {
+            Console.WriteLine("Draw:{0}", Name);
+        }
+        
+    }
+
+    /// <summary>
+    /// 简单图形类——圆
+    /// </summary>
+    public class Circle : Graphics
+    {
+        public Circle(string name)
+            : base(name)
+        { }
+
+        // 重写父类抽象方法
+        public override void Draw()
+        {
+            Console.WriteLine("Draw:{0}", Name);
+        }
+        
+    }
+
+
+    /// <summary>
+    /// 复杂图形，由一些简单图形组成,这里假设该复杂图形由一个圆两条线组成的复杂图形
+    /// </summary>
+    public class ComplexGraphics : Graphics
+    {
+        private List<Graphics> complexGraphicsList = new List<Graphics>();
+
+        public ComplexGraphics(string name)
+            : base(name)
+        { }
+
+        /// <summary>
+        /// 复杂图形的画法
+        /// </summary>
+        public override void Draw()
+        {
+            foreach (Graphics g in complexGraphicsList)
+            {
+                g.Draw();
+            }
+        }
+
+        public void Add(Graphics g)
+        {
+            complexGraphicsList.Add(g);
+        }
+        public void Remove(Graphics g)
+        {
+            complexGraphicsList.Remove(g);
+        }
+    }
+
+
+    public class CompositePattern
+    {
+        public static void CompositePatternMain(string[] args)
+        {
+            ComplexGraphics complexGraphics = new ComplexGraphics("一个复杂图形和两条线段组成的复杂图形");
+            complexGraphics.Add(new Line("线段A"));
+            ComplexGraphics CompositeCG = new ComplexGraphics("一个圆和一条线组成的复杂图形");
+            CompositeCG.Add(new Circle("圆"));
+            CompositeCG.Add(new Circle("线段B"));
+            complexGraphics.Add(CompositeCG);
+            Line l = new Line("线段C");
+            complexGraphics.Add(l);
+
+            // 显示复杂图形的画法
+            Console.WriteLine("复杂图形的绘制如下：");
+            Console.WriteLine("---------------------");
+            complexGraphics.Draw();
+            Console.WriteLine("复杂图形绘制完成");
+            Console.WriteLine("---------------------");
+            Console.WriteLine();
+
+            // 移除一个组件再显示复杂图形的画法
+            complexGraphics.Remove(l);
+            Console.WriteLine("移除线段C后，复杂图形的绘制如下：");
+            Console.WriteLine("---------------------");
+            complexGraphics.Draw();
+            Console.WriteLine("复杂图形绘制完成");
+            Console.WriteLine("---------------------");
+            Console.Read();
+        }
+    }
+
+    /*
+     * 组合模式在.NET 中最典型的应用就是应用与WinForms和Web的开发中，在.NET类库中，都为这两个平台提供了很多现有的控件，然而System.Windows.Forms.dll中System.Windows.Forms.Control类就应用了组合模式，因为控件包括Label、TextBox等这样的简单控件，同时也包括GroupBox、DataGrid这样复合的控件，每个控件都需要调用OnPaint方法来进行控件显示，为了表示这种对象之间整体与部分的层次结构，微软把Control类的实现应用了组合模式（确切地说应用了透明式的组合模式）。
+     * **/
+}
---
+++ LeetCodeSrc/DesignModel/DecoratorPattern.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignModel
+{
+    /*
+     *在装饰者模式中各个角色有：
+     *  抽象构件（Phone）角色：给出一个抽象接口，以规范准备接受附加责任的对象。
+     *  具体构件（AppPhone）角色：定义一个将要接收附加责任的类。
+     *  装饰（Dicorator）角色：持有一个构件（Component）对象的实例，并定义一个与抽象构件接口一致的接口。
+     *  具体装饰（Sticker和Accessories）角色：负责给构件对象 ”贴上“附加的责任。
+
+     */
+
+    /*
+     *优点：
+     * 装饰者模式和继承的目的都是扩展对象的功能，但装饰者模式比继承更灵活
+     * 通过使用不同的具体装饰类以及这些类的排列组合，设计师可以创造出很多不同行为的组合
+     * 装饰者模式有很好地可扩展性
+     *缺点
+     * 装饰者模式会导致设计中出现许多小对象，如果过度使用，会让程序变的更复杂。并且更多的对象会是的差错变得困难，特别是这些对象看上去都很像。
+     */
+
+
+    /// <summary>
+    /// 装饰者模式中的抽象组件类
+    /// </summary>
+    public abstract class Phone
+    {
+        public abstract void DoSomething();
+    }
+
+    /// <summary>
+    /// 装饰者模式中的具体组件类
+    /// </summary>
+    public class ApplePhone : Phone
+    {
+        public override void DoSomething()
+        {
+            Console.WriteLine("Applephone's dosomething");
+        }
+    }
+
+    /// <summary>
+    /// 装饰抽象类，要让装饰完全取代抽象组件，必须继承自Phone
+    /// </summary>
+    public abstract class Decorator : Phone
+    {
+        private Phone phone;
+
+        public Decorator(Phone p)
+        {
+            this.phone = p;
+        }
+
+        public override void DoSomething()
+        {
+            if (phone != null)
+            {
+                phone.DoSomething();
+            }
+        }
+    }
+
+    /// <summary>
+    /// 贴膜
+    /// </summary>
+    public class Sticker : Decorator
+    {
+        public Sticker(Phone p) :  base(p)
+        {
+
+        }
+
+        public override void DoSomething()
+        {
+            base.DoSomething();
+
+            //add new function
+            AddSticker();
+            
+        }
+
+        public void AddSticker()
+        {
+            Console.WriteLine("add sticker");
+        }
+    }
+
+    public class DecoratorPattern
+    {
+        public static void DecoratorPatternMain()
+        {
+            Phone phone = new ApplePhone();
+
+            Decorator applePhoneWithSticker = new Sticker(phone);
+            applePhoneWithSticker.DoSomething();
+        }
+    }
+}
